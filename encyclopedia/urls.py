ae070d2da9e0660289bfcbf19dd0e0f3a84dd9a0
rosa <rosalin.juniati@gmail.com>
2025-09-15 21:42:35 +0700

    solve spec1  entry page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    #req1 to add wiki/TITLE
+    path(f"wiki/<str:title>",views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
 from . import util
-
+# add markdown to change md to html
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request,title):
+    content=util.get_entry(title)
+    
+    return render(request, "encyclopedia/entry.html",{
+        "html_content": markdown2.markdown(content),
+        "title":title
+    })
