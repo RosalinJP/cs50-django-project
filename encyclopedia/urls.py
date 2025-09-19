e1276c6d1b4a1de642750e5412307d69d7a98c57
rosa <rosalin.juniati@gmail.com>
2025-09-19 17:03:11 +0700

    Solve spec 3, SEARCH query

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     #req1 to add wiki/TITLE
-    path(f"wiki/<str:title>",views.entry, name="entry")
+    path("wiki/search", views.search, name="search"),
+    path("wiki/<str:title>",views.entry, name="entry"),
+    
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
 
 from . import util
 # add markdown to change md to html
@@ -16,3 +17,37 @@ def entry(request,title):
         "html_content": markdown2.markdown(content),
         "title":title
     })
+
+def search(request):
+    if request.method == "POST":
+        query = request.POST.get("q","").strip()
+        entries = util.list_entries()
+        # Case 1: exact match → redirect
+        if query.lower() in [entry.lower() for entry in entries]:
+            return redirect(reverse("entry", kwargs={"title": query}))
+
+        # Case 2: partial matches
+        else:
+                matches = [entry for entry in entries if query.lower() in entry.lower()]
+                if matches:
+                     return render(request,"encyclopedia/search.html",{
+                        "query":query,
+                        "entries":matches
+                         } )
+            
+
+            
+        # Case 3: show results page
+        return render(request,"encyclopedia/index.html",{
+            "message":"no result found"
+        })
+    
+
+
+
+            
+
+
+
+
+        
